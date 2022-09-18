4e6682cfe4e27fd113519ab5783eca36e76ea378
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-09-18 19:48:48 +0300

    fighter earnings algo

---
+++ python/fighter_earnings.py
@@ -0,0 +1,20 @@
+def count_highest_earnings(fights_earnings: list):
+    
+    if len(fights_earnings) == 0:
+        return 0
+    
+    if len(fights_earnings) == 1:
+        return fights_earnings[0]
+
+    earnings = 0
+    while len(fights_earnings) != 0:
+        match1_earnings = fights_earnings.pop(0)
+        match2_earnings = fights_earnings.pop(0)
+        earnings += max(match1_earnings, match2_earnings)
+    
+    return earnings
+
+
+if __name__ == '__main__':
+    input = [1, 2, 4, 5, 56, 6]
+    print(count_highest_earnings(input))
