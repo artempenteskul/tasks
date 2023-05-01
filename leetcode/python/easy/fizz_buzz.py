15ae14fe8180dc8f8aac2a2e8d5d53dcdd4fb21b
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-05-01 22:10:26 +0300

    refactoring

--- leetcode/python/easy/fizz_buzz.py
+++ leetcode/python/easy/fizz_buzz.py
@@ -1,23 +1,23 @@
 # task number - 412
 
+
 from typing import List
 
 
-class Solution:
-    @staticmethod
-    def fizz_buzz(n: int) -> List[str]:
-        res_values = []
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                res_values.append('FizzBuzz')
-            elif i % 3 == 0:
-                res_values.append('Fizz')
-            elif i % 5 == 0:
-                res_values.append('Buzz')
-            else:
-                res_values.append(str(i))
-        return res_values
+def fizz_buzz(n: int) -> List[str]:
+    res_values = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            res_values.append('FizzBuzz')
+        elif i % 3 == 0:
+            res_values.append('Fizz')
+        elif i % 5 == 0:
+            res_values.append('Buzz')
+        else:
+            res_values.append(str(i))
+    return res_values
 
 
 if __name__ == '__main__':
-    print(Solution.fizz_buzz(3))  # [1, 2, 'Fizz']
+    print(fizz_buzz(3))  # ['1', '2', 'Fizz']
+    print(fizz_buzz(11))  # ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11']
--- leetcode/python/easy/palindrome_number.py
+++ leetcode/python/easy/palindrome_number.py
@@ -1,16 +1,13 @@
 # task number - 9
 
-class Solution:
-    @staticmethod
-    def is_palindrome(x: int) -> bool:
-        if str(x) == str(x)[::-1]:
-            return True
-        return False
+
+def is_palindrome(x: int) -> bool:
+    if str(x) == str(x)[::-1]:
+        return True
+    return False
 
 
 if __name__ == '__main__':
-    s = Solution()
-
-    print(s.is_palindrome(121))  # True
-    print(s.is_palindrome(-121))  # False
-    print(s.is_palindrome(10))  # False
+    print(is_palindrome(121))  # True
+    print(is_palindrome(-121))  # False
+    print(is_palindrome(10))  # False
