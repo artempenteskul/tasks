61a6d1fc5eb7bc90a59750ecc087c4c40813f279
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-05-01 23:53:19 +0300

    refactoring

--- leetcode/python/easy/merge_two_sorted_lists.py
+++ leetcode/python/easy/merge_two_sorted_lists.py
@@ -1,3 +1,6 @@
+# task number - 21
+
+
 from typing import Optional
 
 
@@ -7,51 +10,32 @@ class ListNode:
         self.next = next
 
 
-class Solution:
-    @staticmethod
-    def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
+def merge_two_sorted_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    head_node = dummy_node = ListNode()
 
-        temp_node = ListNode()
-
+    while list1 and list2:
         if list1.val <= list2.val:
-            head_node = ListNode(list1.val, temp_node)
+            dummy_node.next = list1
             list1 = list1.next
         else:
-            head_node = ListNode(list2.val, temp_node)
+            dummy_node.next = list2
             list2 = list2.next
 
-        while list1 is not None or list2 is not None:
-            if list1 is None:
-                temp_node.val = list2.val
-                list2 = list2.next
-            elif list2 is None:
-                temp_node.val = list1.val
-                list1 = list1.next
-            else:
-                if list1.val <= list2.val:
-                    temp_node.val = list1.val
-                    list1 = list1.next
-                else:
-                    temp_node.val = list2.val
-                    list2 = list2.next
+        dummy_node = dummy_node.next
 
-            if list1 is not None or list2 is not None:
-                temp_node.next = ListNode()
-                temp_node = temp_node.next
+    if list1:
+        dummy_node.next = list1
 
-        return head_node
+    if list2:
+        dummy_node.next = list2
+
+    return head_node.next
 
 
 if __name__ == '__main__':
     l1 = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3)))
     l2 = ListNode(val=1, next=ListNode(val=3, next=ListNode(val=4)))
-    res = Solution.merge_two_lists(l1, l2)
+    res = merge_two_sorted_lists(l1, l2)  # [1, 1, 2, 3, 3, 4]
 
     res_list = []
     while res:
@@ -59,3 +43,33 @@ if __name__ == '__main__':
         res = res.next
 
     print(res_list)
+
+
+# def merge_two_sorted_lists_2(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#     head_node = None
+#     temp_node = ListNode()
+#
+#     while list1 or list2:
+#         if not list1:
+#             current_value = list2.val
+#             list2 = list2.next
+#         elif not list2:
+#             current_value = list1.val
+#             list1 = list1.next
+#         else:
+#             if list1.val <= list2.val:
+#                 current_value = list1.val
+#                 list1 = list1.next
+#             else:
+#                 current_value = list2.val
+#                 list2 = list2.next
+#
+#         if head_node is None:
+#             head_node = ListNode(current_value, temp_node)
+#         else:
+#             temp_node.val = current_value
+#             if list1 or list2:
+#                 temp_node.next = ListNode()
+#                 temp_node = temp_node.next
+#
+#     return head_node
