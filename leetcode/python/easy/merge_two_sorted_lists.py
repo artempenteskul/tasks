19f6a4cf7dcb452b07929ed9c450c2f1b41b6355
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-06-13 20:58:44 +0300

    refactoring

--- leetcode/python/easy/merge_two_sorted_lists.py
+++ leetcode/python/easy/merge_two_sorted_lists.py
@@ -43,33 +43,3 @@ if __name__ == '__main__':
         res = res.next
 
     print(res_list)
-
-
-# def merge_two_sorted_lists_2(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-#     head_node = None
-#     temp_node = ListNode()
-#
-#     while list1 or list2:
-#         if not list1:
-#             current_value = list2.val
-#             list2 = list2.next
-#         elif not list2:
-#             current_value = list1.val
-#             list1 = list1.next
-#         else:
-#             if list1.val <= list2.val:
-#                 current_value = list1.val
-#                 list1 = list1.next
-#             else:
-#                 current_value = list2.val
-#                 list2 = list2.next
-#
-#         if head_node is None:
-#             head_node = ListNode(current_value, temp_node)
-#         else:
-#             temp_node.val = current_value
-#             if list1 or list2:
-#                 temp_node.next = ListNode()
-#                 temp_node = temp_node.next
-#
-#     return head_node
