c26e4494be7b2360cc61f0260bf579aab2ebb609
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-06-27 19:35:06 +0300

    removed blank line

--- leetcode/python/easy/remove_duplicates_from_sorted_array.py
+++ leetcode/python/easy/remove_duplicates_from_sorted_array.py
@@ -20,4 +20,3 @@ def remove_duplicates_from_sorted_array(nums: List[int]) -> int:
 if __name__ == '__main__':
     print(remove_duplicates_from_sorted_array([1, 1, 2]))  # 2
     print(remove_duplicates_from_sorted_array([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))  # 5
-
