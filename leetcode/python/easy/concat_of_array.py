2c4660fd01523df2badb1c9e2a12431d9eb61d7e
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-04-16 21:30:45 +0300

    solved #1929 concat_of_array problem

---
+++ leetcode/python/easy/concat_of_array.py
@@ -0,0 +1,11 @@
+# task number - 1929
+
+
+def concat_array(nums: list[int]) -> list[int]:
+    return nums * 2
+
+
+if __name__ == '__main__':
+    input_1 = [1, 2, 3]
+    output_1 = concat_array(input_1)
+    print(f'Result of input_1 - {output_1}')
