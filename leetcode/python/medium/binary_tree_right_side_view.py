5b783d2cf5cd1763b176fd6cc2480838951549f8
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-09-14 20:19:10 +0300

    solved #199 binary_tree_right_side_view problem

---
+++ leetcode/python/medium/binary_tree_right_side_view.py
@@ -0,0 +1,71 @@
+# task number - 199
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left: 'TreeNode' = None, right: 'TreeNode' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# def right_side_view(root: TreeNode) -> list[int]:
+#     if not root:
+#         return []
+#
+#     view = []
+#     queue = [root]
+#     current_level_size = 1
+#     next_level_size = 0
+#     current_level_val = None
+#
+#     while queue:
+#         for _ in range(current_level_size):
+#             node = queue.pop(0)
+#             current_level_val = node.val
+#
+#             if node.left:
+#                 queue.append(node.left)
+#                 next_level_size += 1
+#
+#             if node.right:
+#                 queue.append(node.right)
+#                 next_level_size += 1
+#
+#         view.append(current_level_val)
+#         current_level_size = next_level_size
+#         next_level_size = 0
+#
+#     return view
+
+
+def right_side_view(root: TreeNode) -> list[int]:
+    if not root:
+        return []
+
+    view = []
+    queue = [root]
+    current_level_val = None
+
+    while queue:
+        current_level_len = len(queue)
+        for _ in range(current_level_len):
+            node = queue.pop(0)
+            current_level_val = node.val
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        view.append(current_level_val)
+
+    return view
+
+
+if __name__ == '__main__':
+    input_root = TreeNode(1, left=TreeNode(2, left=TreeNode(5)), right=TreeNode(3, right=TreeNode(4)))
+    print(f'Right side view for input binary tree - {right_side_view(input_root)}')
+
+    input_root = TreeNode(1, right=TreeNode(3))
+    print(f'Right side view for input binary tree - {right_side_view(input_root)}')
