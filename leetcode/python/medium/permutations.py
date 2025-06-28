8e6ba53fc1518b4ae26e50caff3bb4f748ca720a
Artem Penteskul <artemiy.penteskul@gmail.com>
2025-06-28 17:50:10 +0300

    solved permutations problem

---
+++ leetcode/python/medium/permutations.py
@@ -0,0 +1,27 @@
+# task number - 46
+# topics - array, backtracking
+
+
+def permute(nums: list[int]) -> list[list[int]]:
+    res = []
+
+    def backtrack(current: list[int]):
+        if len(current) == len(nums):
+            res.append(current)
+        else:
+            for num in nums:
+                if num not in current:
+                    new_current = current + [num]
+                    backtrack(new_current)
+
+    backtrack([])
+
+    return res
+
+
+if __name__ == '__main__':
+    input_nums = [1, 2, 3]
+    print(f'Permutations - {permute(input_nums)}')  # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+    input_nums = [0, 1]
+    print(f'Permutations - {permute(input_nums)}')  # [[0, 1], [1, 0]]
